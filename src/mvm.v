9507110d01df2fd6010d35d5c5856e15b223cae1
Aidan Zinn <aidanzinn@gmail.com>
2023-11-01 13:00:12 -0700

    Fixed test code

--- src/mvm.v
+++ src/mvm.v
@@ -1,80 +1,94 @@
 module MVM_Accelerator (
-    input start,                    // Signal to start MVM
-    input clk,                      // Clock
-    input rst_n,                    // Reset
-    input [1:0] row_val,            // CSR row pointers for 4x4 matrix
-    input [7:0] value,              // CSR values for 4x4 matrix (assuming max 16 non-zero values)
-    input [1:0] column_val,         // CSR column indices for 4x4 matrix (assuming max 16 non-zero values)
+    input start,                    
+    input clk,                      
+    input rst_n,                    
+    input [1:0] row_val,            
+    input [7:0] value,              
+    input [1:0] column_val,         
     input sending_CPU,
     input done_list,
 
-    output reg [7:0] output_val,    // Resultant output after MVM
+    output reg [7:0] output_val,    
     output reg sending_out,
     output reg FETCH_ready
 );
 
-reg [1:0] row_pointers[8:0];       // CSR row pointers for 4x4 matrix
-reg [7:0] values[8:0];             // CSR values for 4x4 matrix (assuming max 16 non-zero values)
-reg [1:0] column_indices[8:0];     // CSR column indices for 4x4 matrix (assuming max 16 non-zero values)
-reg [7:0] result[2:0];              // Resultant output after MVM
-reg [2:0] spike_train = 3'b000;    // 4-input spike train
+reg [1:0] row_pointers[8:0];       
+reg [7:0] values[8:0];             
+reg [1:0] column_indices[8:0];     
+reg [7:0] result[2:0];              
+reg [2:0] spike_train = 3'b000;    
 
-parameter [2:0] IDLE        = 3'b000,   // states
-                TRANSMIT    = 3'b001,   // transmit computed vector
-                COMPUTE     = 3'b010,   // compute matrix vectormultiplication
-                FETCH_CSR   = 3'b011,   // fetch CSR formatted matrix
-                FETCH_TRAIN = 3'b100;   // fetch spiketrain
+parameter [2:0] IDLE        = 3'b000,   
+                TRANSMIT    = 3'b001,   
+                COMPUTE     = 3'b010,   
+                FETCH_CSR   = 3'b011,   
+                FETCH_TRAIN = 3'b100;   
 
 reg [2:0] state = IDLE;
-reg [1:0] current_row = 0;  // Current row being processed
+reg [1:0] current_row = 0;  
 reg [3:0] i = 0;    
-reg [1:0] j = 0;            // counters
+reg [1:0] j = 0;            
   
-reg [7:0] interval;         // intermediate value for calculation
+reg [7:0] interval;
 
 always @(posedge clk or negedge rst_n) begin
     if (!rst_n) begin
-        state <= IDLE;          // reset register values on reset
+        state <= IDLE;          
         current_row <= 0;
+        interval <= 0;
         i <= 0;
         j <= 0;
         spike_train <= 3'b000;
     end else begin
         case (state)
-
+            /*----------------------------------------
+            *       Initial State
+            *---------------------------------------*/
             IDLE: begin
-                if (start) begin                    // use start(ena) to begin loading data from CPU
+                current_row <= 0;
+                interval <= 0;
+                i <= 0;
+                j <= 0;
+                spike_train <= 3'b000;
+                sending_out <= 1;
+                if (start) begin                    
                     state <= FETCH_CSR;
                   
                 end
             end
-
+            /*----------------------------------------
+            *       Get Time Multiplexed Inputs
+            *---------------------------------------*/
             FETCH_CSR: begin
-                
-                FETCH_ready <= 1;                       // CPU done loading flag
-                if (sending_CPU) begin                  // if CPU is sending new data
-                    FETCH_ready <= 0;                   // not ready for new data
-                    row_pointers[i] <= row_val;         // load values in CSR format (row) (col) (value)
+                FETCH_ready <= 1;
+                if (done_list) begin           
+                    FETCH_ready <= 0;                   
+                    state <= FETCH_TRAIN;               
+                    i <= 0;                       
+                end else if (sending_CPU) begin                  
+                    FETCH_ready <= 0;                   
+                    row_pointers[i] <= row_val;         
                     column_indices[i] <= column_val;    
                     values[i] <= value;
-                    i <= i+1;                           // iterate index for arrays
-                end else if (done_list) begin           // if done sending CSR matrix (flag from CPU)
-                    FETCH_ready <= 0;                   // not ready for spiketrain yet
-                    state <= FETCH_TRAIN;               // go to get the spike train
-                    i <= 0;                             // reset index counter
+                    i <= i + 1;                           
                 end   
             end     
-
+            /*----------------------------------------
+            *       Get Spiking Inputs
+            *---------------------------------------*/
             FETCH_TRAIN: begin
 
-                FETCH_ready <= 1;                       // ready for new data
+                FETCH_ready <= 1;                       
                 if (sending_CPU) begin
-                    spike_train <= value[3:0];          // load in spike train
-                    FETCH_ready <= 1;                   // ready to get data again
-                    state <= COMPUTE;                   // compute 
+                    spike_train <= value[3:0];          
+                    FETCH_ready <= 1;                   
+                    state <= COMPUTE;                    
                 end            
             end
-
+            /*----------------------------------------
+            *       Do Multiplication
+            *---------------------------------------*/
             COMPUTE: begin                
                 if (row_pointers[i] == current_row) begin
                     interval <= ((spike_train[column_indices[i]])*(values[i])) + interval;
@@ -91,13 +105,15 @@ always @(posedge clk or negedge rst_n) begin
                     current_row <= current_row + 1;
                 end                                                 
             end
-
+            /*----------------------------------------
+            *       Send Outputs
+            *---------------------------------------*/
             TRANSMIT: begin               
                 output_val <= result[j];
                 sending_out <= sending_out^1'b1;
                 j <= j+1;
 
-                if (i>2) begin
+                if (j>2) begin
                     state <= IDLE;  
                     j <= 0; 
                end 
--- src/test.py
+++ src/test.py
@@ -5,7 +5,7 @@ from cocotb.triggers import RisingEdge, FallingEdge, Timer, ClockCycles
 @cocotb.test()
 async def test_mvm(dut):
 
-    dut._log.info("start")
+    dut._log.info("TEST CASE 1")
 
     clock = Clock(dut.clk, 1, units= 'ns')
     cocotb.start_soon(clock.start())
@@ -13,78 +13,181 @@ async def test_mvm(dut):
     dut.rst_n.value = 0
     await ClockCycles(dut.clk, 10)
     dut.rst_n.value = 1
+    dut.uio_in.value = 0b00101000
+
     dut.ena.value = 1
+
+    await ClockCycles(dut.clk, 1)
+    dut.uio_in.value = 0b00100000
+    await ClockCycles(dut.clk, 1)
+
+    
 
     VALUES = [32,14,15,7]
     ROWS = [0,1,1,2]
     COLS = [2,0,2,1]
-    i = 0
 
-    ui_in = [0]*8
-    ui_in[7] = 0
-    ui_in[6] = 0
-    ui_in[5] = 1
-    ui_in[4] = 0
-    ui_in[3] = 1
-    ui_in[2] = 0
-    ui_in[1] = 0
-    ui_in[0] = 0
-
-    dut.ui_in.value = VALUES[i]
+    dut.ui_in.value = VALUES[0]
     dut.uio_in.value = 0b00101000
-    dut._log.info(list)
+    dut.ena.value = 1
 
-    await ClockCycles(dut.clk, 10)
-    
+    await ClockCycles(dut.clk, 1)
+    dut.uio_in.value = 0b00100000
+    await ClockCycles(dut.clk, 1)
+
     i=1
-    ui_in[7] = 0
-    ui_in[6] = 1
-    ui_in[5] = 0
-    ui_in[4] = 0
-    ui_in[3] = 1
-    ui_in[2] = 0
-    ui_in[1] = 0
-    ui_in[0] = 0
     
-    dut.ui_in.value = VALUES[i]
+    dut.ui_in.value = VALUES[1]
     dut.uio_in.value = 0b01001000
     
 
-    await ClockCycles(dut.clk, 10)
+    await ClockCycles(dut.clk, 1)
+    dut.uio_in.value = 0b00100000
+    await ClockCycles(dut.clk, 1)
 
     i=2
-    ui_in[7] = 0
-    ui_in[6] = 1
-    ui_in[5] = 1
-    ui_in[4] = 0
-    ui_in[3] = 1
-    ui_in[2] = 0
-    ui_in[1] = 0
-    ui_in[0] = 0
     
-    dut.ui_in.value = VALUES[i]
+    dut.ui_in.value = VALUES[2]
     dut.uio_in.value = 0b01101000
 
-    await ClockCycles(dut.clk, 10)
+    await ClockCycles(dut.clk, 1)
+    dut.uio_in.value = 0b00100000
+    await ClockCycles(dut.clk, 1)
 
     i=3
-    ui_in[7] = 1
-    ui_in[6] = 0
-    ui_in[5] = 0
-    ui_in[4] = 1
-    ui_in[3] = 1
-    ui_in[2] = 0
-    ui_in[0] = 0
-    ui_in[1] = 0
 
     
-    dut.ui_in.value = VALUES[i]
+    dut.ui_in.value = VALUES[3]
     dut.uio_in.value = 0b10011000
 
-    
+    await ClockCycles(dut.clk, 1)
+    dut.uio_in.value = 0b00100000
+    await ClockCycles(dut.clk, 1)
 
+    dut.ui_in.value = 0b101
+    dut.uio_in.value = 0b00001100
+
+    dut.ena.value = 0
+
+
+    for _ in range(100):    
+        await RisingEdge(dut.clk)
+
+
+    
+    dut._log.info("TEST CASE 2")
+
+    dut.rst_n.value = 0
+    await ClockCycles(dut.clk, 10)
+    dut.rst_n.value = 1
+    dut.uio_in.value = 0b00101000
+
+    dut.uio_in.value = 0b00101000
+    dut.ena.value = 1
+
+    await ClockCycles(dut.clk, 1)
+    dut.uio_in.value = 0b00100000
+    await ClockCycles(dut.clk, 1)
+
+    VALUES = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    ROWS = [0, 0, 0, 1, 1, 1, 2, 2, 2,]
+    COLS = [0, 0, 0, 1, 1, 1, 2, 2, 2,]
+
+    dut.ui_in.value = VALUES[0]
+    dut.uio_in.value = 0b00001000
+    dut.ena.value = 1
+
+    await ClockCycles(dut.clk, 1)
+    dut.uio_in.value = 0b00000000
+    await ClockCycles(dut.clk, 1)
+
+    dut.ui_in.value = VALUES[1]
+    dut.uio_in.value = 0b00001000
+    dut.ena.value = 1
+
+    await ClockCycles(dut.clk, 1)
+    dut.uio_in.value = 0b00000000
+    await ClockCycles(dut.clk, 1)
+
+    dut.ui_in.value = VALUES[2]
+    dut.uio_in.value = 0b00001000
+    dut.ena.value = 1
+
+    await ClockCycles(dut.clk, 1)
+    dut.uio_in.value = 0b00000000
+    await ClockCycles(dut.clk, 1)
+
+    dut.ui_in.value = VALUES[3]
+    dut.uio_in.value = 0b01011000
+    dut.ena.value = 1
+
+    await ClockCycles(dut.clk, 1)
+    dut.uio_in.value = 0b00000000
+    await ClockCycles(dut.clk, 1)
+
+    dut.ui_in.value = VALUES[4]
+    dut.uio_in.value = 0b01011000
+    dut.ena.value = 1
+
+    await ClockCycles(dut.clk, 1)
+    dut.uio_in.value = 0b00000000
+    await ClockCycles(dut.clk, 1)
+
+    dut.ui_in.value = VALUES[5]
+    dut.uio_in.value = 0b01011000
+    dut.ena.value = 1
+
+    await ClockCycles(dut.clk, 1)
+    dut.uio_in.value = 0b00000000
+    await ClockCycles(dut.clk, 1)
+
+    dut.ui_in.value = VALUES[6]
+    dut.uio_in.value = 0b10101000
+    dut.ena.value = 1
+
+    await ClockCycles(dut.clk, 1)
+    dut.uio_in.value = 0b00000000
+    await ClockCycles(dut.clk, 1)
+
+    dut.ui_in.value = VALUES[7]
+    dut.uio_in.value = 0b10101000
+    dut.ena.value = 1
+
+    await ClockCycles(dut.clk, 1)
+    dut.uio_in.value = 0b00000000
+    await ClockCycles(dut.clk, 1)
+
+    dut.ui_in.value = VALUES[8]
+    dut.uio_in.value = 0b10101000
+    dut.ena.value = 1
+
+    await ClockCycles(dut.clk, 1)
+    dut.uio_in.value = 0b00000000
+    await ClockCycles(dut.clk, 1)
+
+    dut.ui_in.value = 0b111
+    dut.uio_in.value = 0b00001100
+
+    await ClockCycles(dut.clk, 5)
+
+    temp = dut.uio_out.value
+
+    # assert  dut.uo_out.value == 6, "Failed first number"
+    
+    dut._log.info(temp)
+
+    dut.ena.value = 0
 
     for _ in range(100):    # runs for 100 clk cycles
         await RisingEdge(dut.clk)
+ 
+    
+
+    
+
+
     
     dut._log.info("Finished Test!")
+
+
+
